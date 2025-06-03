2f2167c6cd83b89070739a36dd8c37fdda2eed91
CAIQUE BATISTA LINO <caiquelino6183@digitalcollege.internal>
2025-06-03 20:16:11 -0300

    Comitando html e bootstrap

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function converterCtoF(tempC) {
     const tempF = (tempC*9/5) + 32
     console.log(`Temperatura = ${tempF} F`);
+    return tempF
 }
 
-converterCtoF(30)
+function converter(){
+    console.log('Ei Clicou!')
+    let tempC = document.getElementById('tempC').value
+    console.log(`Temp °C: ${tempC}`);
+
+    /*Chamando a Função que converte de C para F */
+    const resultado = converterCtoF(tempC)
+
+    document.getElementById('resultado').innerHTML = resultado
+    
+}
